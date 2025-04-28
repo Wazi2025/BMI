6fd28ad9d7e0f6c0444e839e201e321b5ec41893
Jan Leirvaag <lja022025@gmail.com>
2025-04-28 13:45:44 +0200

    new

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BMI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double heightFloat = 0;
+        double weightFloat = 0;
+        bool convertHeight = false;
+        bool convertWeight = false;
+        double bmi;
+
+
+        Console.WriteLine("Please type in your height (in cm): ");
+        string height = Console.ReadLine();
+        Console.WriteLine("Please type in your weight (in kg): ");
+        string weight = Console.ReadLine();
+
+        convertHeight = double.TryParse(height, out heightFloat);
+
+        convertWeight = double.TryParse(weight, out weightFloat);
+
+        bmi = weightFloat / (heightFloat / 100 * heightFloat / 100);
+
+        Console.WriteLine($"Your BMI is: {bmi:F2}");
+
+    }
+}
